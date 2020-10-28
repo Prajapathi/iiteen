710f4c108723b8650200a3f98ec6de169267d5de
AnjaliPatle <anjalipatle1417@gmail.com>
2020-10-28 17:42:34 +0530

    Add loading for upload

--- src/adminPanel/Paper.js
+++ src/adminPanel/Paper.js
@@ -3,12 +3,15 @@ import firebase from 'firebase'
 import {Link,useLocation} from "react-router-dom";
 import PaperInfo from './elements/PaperInfo'
 import InstructionInfo from './elements/InstructionInfo'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 export default function Paper() {
     const [numberQ,setNumberQ]=React.useState(0)
     const [paperInfo,setPaperInfo]=React.useState([]);
     const [instructionInfo,setInstructionInfo]=React.useState([]);
     const [subjectiveClass,setSubjectiveClass]=React.useState();
+    const[loading,setLoading]=React.useState(false)
     const location = useLocation();
 
     const addPaper=()=>{
@@ -17,28 +20,38 @@ export default function Paper() {
         db.settings({
             timestampsInSnapshots: true
         });
+        setLoading(true)
         const userRef = db.collection("Trash/questionPaper/Questions").add({
             data
-        });  
+        }).then((res)=>{
+            setLoading(false);
+            
+        }).catch((error)=>{
+            console.log("Error loading the document: ",error)
+        })  
     }
     return (
         <div>
-            <PaperInfo sendNumberQ={setNumberQ} sendInfo={setPaperInfo} subjective={location.state.subjective} sendSubjectiveClass={setSubjectiveClass}/>
-            {
-                !(location.state.subjective)?<InstructionInfo sendInfo={setInstructionInfo}/>:null
+            {loading==true?<CircularProgress style={{margin:'25% 50%'}}/>:
+                <>
+                <PaperInfo sendNumberQ={setNumberQ} sendInfo={setPaperInfo} subjective={location.state.subjective} sendSubjectiveClass={setSubjectiveClass}/>
+                {
+                    !(location.state.subjective)?<InstructionInfo sendInfo={setInstructionInfo}/>:null
+                }
+                {/*  <Link to={{ pathname: '/Questions', state: { number:numberQ,subjective:location.state.subjective,paperType:location.state.paperType} }}> */}
+                    <button style={{width:'60%',
+                    margin:'0px 20% 20px 20%',
+                    background:'#388cf2',
+                    color:'white',
+                    border:'1px solid white',
+                    borderRadius:'20px'
+                    }}
+                    onClick={addPaper}>
+                        Continue
+                    </button>
+                {/* </Link> */}
+                </>
             }
-            <Link to={{ pathname: '/Questions', state: { number:numberQ,subjective:location.state.subjective,paperType:location.state.paperType} }}>
-                <button style={{width:'60%',
-                margin:'0px 20% 20px 20%',
-                background:'#388cf2',
-                color:'white',
-                border:'1px solid white',
-                borderRadius:'20px'
-                }}
-                onClick={addPaper}>
-                    Continue
-                </button>
-            </Link>
         </div>
     )
 }
